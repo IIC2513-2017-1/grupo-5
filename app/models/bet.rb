04b2cdc53d0c23280995cd7a9fb34dfd182c0ff6
Pablo <pfnieto@uc.cl>
2017-04-30 20:36:03 -0300

    validations and selectors

--- app/models/bet.rb
+++ app/models/bet.rb
@@ -1,5 +1,5 @@
 class Bet < ApplicationRecord
-  validates :ammount, presence: true, allow_blank: false
+  validates :ammount, :numericality => {:greater_than_or_equal_to => 0}, presence: true, allow_blank: false
   validates :bet_state, presence: true, allow_blank: false
   validates :match_id, presence: true, allow_blank: false
   validates :user_id, presence: true, allow_blank: false
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  validates :coins, :numericality => {:greater_than_or_equal_to => 0}
+  validates :role, :numericality => {:greater_than_or_equal_to => 0}
   validates :password, presence: true, length: { minimum: 5 },
                        confirmation: true, allow_blank: false
   validates :email, presence: true, uniqueness: true, allow_blank: false,
