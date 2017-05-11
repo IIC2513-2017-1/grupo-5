30e6deac8ec91d81f988baa7132f88f836142f6c
Pablo <pfnieto@uc.cl>
2017-05-11 19:36:57 -0300

    secure passwords

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_secure_password
+  
   validates :coins, :numericality => {:greater_than_or_equal_to => 0}
   validates :role, :numericality => {:greater_than_or_equal_to => 0}
   validates :password, presence: true, length: { minimum: 5 },
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170430224833) do
+ActiveRecord::Schema.define(version: 20170511223140) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -81,14 +81,14 @@ ActiveRecord::Schema.define(version: 20170430224833) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.text     "email",      null: false
+    t.text     "email",           null: false
     t.text     "first_name"
     t.text     "last_name"
-    t.text     "password",   null: false
-    t.integer  "coins",      null: false
-    t.integer  "role",       null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.integer  "coins",           null: false
+    t.integer  "role",            null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
   end
 
