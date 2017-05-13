1aea1b7dc6e3d681f488b0b24f3289ef4aa20a72
Pablo <pfnieto@uc.cl>
2017-05-13 17:05:11 -0300

    user current session for creating events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -69,6 +69,6 @@ class EventsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def event_params
-      params.require(:event).permit(:event_type, :user_id, :private, :date_start, :date_end)
+      params.require(:event).permit(:event_type, :private, :date_start, :date_end).merge(user_id: current_user.id)
     end
 end
