8f451c9f2ae27c395ee2e6cc1263c90b17fea90e
Pablo <pfnieto@uc.cl>
2017-05-13 20:22:47 -0300

    css template from pioller

--- db/migrate/20170513211336_delete_player_id_from_participations.rb
+++ db/migrate/20170513211336_delete_player_id_from_participations.rb
@@ -1,6 +1,6 @@
 class DeletePlayerIdFromParticipations < ActiveRecord::Migration[5.0]
   def change
-    remove_index :participations, :player_id
-    remove_column :participations, :player_id
+    #remove_index :participations, :player_id
+    #remove_column :participations, :player_id
   end
 end
