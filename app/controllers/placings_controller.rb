960344b8d686ddbeb62668e8005577d930aef753
Pablo <pfnieto@uc.cl>
2017-05-28 17:30:06 -0400

    begin distributing earnings

--- app/controllers/placings_controller.rb
+++ app/controllers/placings_controller.rb
@@ -2,7 +2,22 @@ class PlacingsController < ApplicationController
   def new; end
 
   def create
-    @match_id = Match.find(params[:match_id])
+    ## Find teams and bets
+    @match = Match.find(params[:match_id])
+    @teams = Team.joins(:participations).where(['participations.match_id = ?', @match.id]).order(placing: :asc)
+    @bets = Bet.where(match_id: @match.id)
+    coins = @bets.sum(:ammount)
+    ## Close bets and distribute coins
+    @bets.each do |bet|
+
+      bet.bet_state = 1
+      bet.save
+    end
+    ## Close match
+    @match.state = 1
+    @match.save
+    redirect_to events_path(@match.event_id), notice: 'Match as succesfully closed and earnings were distributed'
+
 
   end
 end
