de9cb0eedffa9c86942212c6a0aa7dc9bb32a725
Antonio López <alopez7@uc.cl>
2017-05-28 18:16:59 -0400

    Funcionando invitaciones completamente

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -8,4 +8,15 @@ module EventsHelper
       link_to 'Invite', event_invitations_path(event_id: event.id, user_id: user.id), method: 'post'
     end
   end
+
+  def need_request?(event)
+    return false unless event.private
+    return false unless current_user
+    foll = Follow.where(follower_id: event.user_id, followed_id: current_user.id).first
+    if foll
+      return false
+    else
+      return true
+    end
+  end
 end
