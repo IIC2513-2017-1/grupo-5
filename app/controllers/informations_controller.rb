0c4814109f46da03d5d138742212d4a4dcf74c36
Pablo <pfnieto@uc.cl>
2017-05-28 21:48:09 -0400

    history in xlsx

--- app/controllers/informations_controller.rb
+++ app/controllers/informations_controller.rb
@@ -5,8 +5,9 @@ class InformationsController < ApplicationController
     @match = Match.find(params[:match_id])
     @event = Event.find(params[:event_id])
     @user = User.find(@event.user_id)
-    @bets = Bet.where(match_id: @match.id)
-    render xlsx: 'create', template: 'information/create', filename: "results.xlsx"
+    @bets = Bet.where(match_id: @match.id).includes(:user, :team)
+    @winner = Team.joins(:participations).where(['participations.match_id = ? AND participations.placing = ?', @match.id, 1]).first
+    render xlsx: 'create', template: '/informations/create.xlsx.axlsx', filename: "history.xlsx"
       #redirect_to root_path
   end
 end
