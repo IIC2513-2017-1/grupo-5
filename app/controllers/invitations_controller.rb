51a84d6b45cb422b7c4bf2d3e738f3c479268497
Antonio López <alopez7@uc.cl>
2017-05-28 20:08:04 -0400

    Working Invitations (#23)
    * Working Invitations
    
    * Funcionando invitaciones completamente
    
    * cambio chico
    
    * Privacidades Hechas

---
+++ app/controllers/invitations_controller.rb
@@ -0,0 +1,47 @@
+class InvitationsController < ApplicationController
+
+  def index
+    @invitations = Invitation.all
+  end
+
+  def new
+    @invitation = Invitation.new
+  end
+
+  def create
+    @invitation = Invitation.new(invitation_params)
+    puts "User"
+    puts @invitation.user_id
+    puts "Event"
+    puts @invitation.event_id
+    @event = Event.where(id: @invitation.event_id).first
+
+    respond_to do |format|
+      if @invitation.save
+        format.html { redirect_to @event, notice: 'Invitation was successfully sended.' }
+        format.json { render :show, status: :created, location: @event }
+      else
+        format.html { redirect_to @event, notice: 'The invitation could not be sended.' }
+        format.json { render json: @event.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+
+  def destroy
+    @invitation = Invitation.find(params[:id])
+    @event = Event.where(id: @invitation.event_id).first
+    @invitation.destroy
+    respond_to do |format|
+      format.html { redirect_to @event, notice: 'Invitation was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def invitation_params
+      params.permit(:event_id, :user_id)
+    end
+end
--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,23 @@
 module EventsHelper
+  def invite_button(user, event)
+    return unless current_user && current_user.role != 1
+    inv = Invitation.where(user_id: user.id, event_id: event.id).first
+    if inv
+      link_to 'Uninvite', invitation_path(inv), method: 'delete'
+    else
+      link_to 'Invite', event_invitations_path(event_id: event.id, user_id: user.id), method: 'post'
+    end
+  end
+
+  def need_request?(event)
+    return false unless event.private
+    return false unless current_user
+    return false unless current_user.id != event.user_id
+    foll = Follow.where(follower_id: event.user_id, followed_id: current_user.id).first
+    if foll
+      return false
+    else
+      return true
+    end
+  end
 end
