8f43b9cfdbb5f2c6645b525d1f2de7955377f499
Pablo <pfnieto@uc.cl>
2017-06-11 19:38:53 -0400

    bets distributed

--- app/controllers/placings_controller.rb
+++ app/controllers/placings_controller.rb
@@ -7,20 +7,20 @@ class PlacingsController < ApplicationController
     @teams = Team.joins(:participations).where(['participations.match_id = ?', @match.id]).order(placing: :desc)
     @bets = Bet.where(match_id: @match.id)
     @coins = @bets.sum(:ammount)
-    @places = Participations.where(match_id: @match.id).maximum(:placing)
-    @total_places = Participations.where(match_id: @match.id).sum(:placing)
-    @temp_money = @coins/@places
+    @places = Participation.where(match_id: @match.id).maximum(:placing)
+    @total_places = Participation.where(match_id: @match.id).sum(:placing)
+    @temp_money = @coins/@total_places
 
-    for i in @places do
+    for i in 0..@places do
       @current_money = @temp_money * (@places - i)
-      @winners = Bets.joins(:participations).where(['participations.team_id = bets.team_id AND participations.placing = ?', i+1])
-      @winners_count = Bets.joins(:participations).where(['participations.team_id = bets.team_id AND participations.placing = ?', i+1]).count
+      @winner_team = Team.joins(:participations).where(['participations.match_id = ? AND participations.placing = ?', @match.id, i+1])
+      @winners = Bet.where(team_id: @winner_team.ids, match_id: @match.id)
+      @winners_count = @winners.count
       @winners.each do |winner|
-        @user = User.find(winner.bets.user_id)
+        @user = User.find(winner.user_id)
         BetsMailer.bets_mail(@user, @match, @current_money/@winners_count).deliver_later
         @user.update_attribute(:coins, @user.coins + @current_money/@winners_count)
-        @bet = Bets.find(winner.bets.id)
-        @bet.update_attribute(:state, 1)
+        winner.update_attribute(:bet_state, 1)
       end
     end
 
