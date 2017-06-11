539161d93202a670eed5cadae5ba34a117b0e0cb
Antonio López <alopez7@uc.cl>
2017-06-10 22:53:32 -0400

    Por fin Entiendo Ajax

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -9,7 +9,12 @@ class FollowsController < ApplicationController
       follower_id: current_user.id
     )
     if followed_relationship.save
-      redirect_to users_path
+      respond_to do |format|
+        format.html {redirect_to users_path}
+        format.json do
+          render json: {followed_id: user.id, follower_id: current_user.id, follow_id: followed_relationship.id}
+        end
+      end
     else
       redirect_to users_path, alert: "Can't follow that user"
     end
@@ -18,6 +23,11 @@ class FollowsController < ApplicationController
   def destroy
     follow = Follow.find(params[:id])
     follow.destroy
-    redirect_to users_path, notice: "You are not following #{follow.followed.email} anymore"
+    respond_to do |format|
+      format.html { redirect_to users_path, notice: "You are not following #{follow.followed.email} anymore" }
+      format.json do
+        render json: {unfollowed_id: follow.followed.id, follower_id: current_user.id}
+      end
+    end
   end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,3 +1,5 @@
+
+
 module UsersHelper
   def following_button(user)
     return unless current_user
@@ -6,9 +8,9 @@ module UsersHelper
       relation.follower_id == current_user.id
     end
     if followed_relation
-      link_to 'Unfollow', follow_path(followed_relation), method: 'delete'
+      link_to 'Unfollow', follow_path(followed_relation), method: 'delete', remote: true, data: { type: 'json', 'user-id': user.id }
     else
-      link_to 'Follow', user_follows_path(user), method: 'post'
+      link_to 'Follow', user_follows_path(user), method: 'post', remote: true, data: { type: 'json', 'user-id': user.id }
     end
   end
 end
