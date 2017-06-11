1f7939ed4cb9c745f4d87348f1794def9519cf38
Antonio López <alopez7@uc.cl>
2017-06-10 23:54:35 -0400

    Por fin Entiendo Ajax (#29)

---
+++ app/assets/javascripts/follows.js
@@ -0,0 +1,24 @@
+
+$(document).on('turbolinks:load', function () {
+  var $users;
+  if (($users = $('.users')).length) {
+    // procesamiento de la respuesta del request Ajax para seguir/deseguir un usuario
+    $users.on('ajax:success', function (e, data) {
+      var $button;
+      // si la respuesta contiene un usuario al que se sigue
+      if (data.followed_id) {
+        $button = $('.user-stream-item [data-user-id="' + data.followed_id + '"]');
+        $button.data('method', 'delete');
+        $button.attr('href', '/follows/' + data.follow_id);
+        $button.text('Unfollow');
+      } else {
+        $button = $('.user-stream-item [data-user-id="' + data.unfollowed_id + '"]');
+        $button.data('method', 'post');
+        $button.attr('href', '/users/' + data.unfollowed_id + '/follows');
+        $button.text('Follow');
+      }
+    }).on('ajax:error', function (e, data) {
+      console.log(data);
+    });
+  }
+});
