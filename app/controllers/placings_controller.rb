304f67dea8e9d32441930a420b7c1b80e0b3bd7a
Pablo Nieto <pfnieto@uc.cl>
2017-06-11 19:41:04 -0400

    New bets formula (#33)
    * first attempt
    
    * bets distributed
    
    * erase unnecessary line

--- app/controllers/placings_controller.rb
+++ app/controllers/placings_controller.rb
@@ -4,19 +4,25 @@ class PlacingsController < ApplicationController
   def create
     ## Find teams and bets
     @match = Match.find(params[:match_id])
-    @teams = Team.joins(:participations).where(['participations.match_id = ?', @match.id]).order(placing: :desc)
     @bets = Bet.where(match_id: @match.id)
-    #coins = @bets.sum(:ammount)
-    #total_place = @team.sum(:placing)
-    #current = 1
-    ## Close bets and distribute coins
-    @bets.each do |bet|
-      @user = User.find(bet.user_id)
-      BetsMailer.bets_mail(@user, @match, bet.ammount).deliver_later
-      @user.update_attribute(:coins, @user.coins + bet.ammount)
-      bet.bet_state = 1
-      bet.save
+    @coins = @bets.sum(:ammount)
+    @places = Participation.where(match_id: @match.id).maximum(:placing)
+    @total_places = Participation.where(match_id: @match.id).sum(:placing)
+    @temp_money = @coins/@total_places
+
+    for i in 0..@places do
+      @current_money = @temp_money * (@places - i)
+      @winner_team = Team.joins(:participations).where(['participations.match_id = ? AND participations.placing = ?', @match.id, i+1])
+      @winners = Bet.where(team_id: @winner_team.ids, match_id: @match.id)
+      @winners_count = @winners.count
+      @winners.each do |winner|
+        @user = User.find(winner.user_id)
+        BetsMailer.bets_mail(@user, @match, @current_money/@winners_count).deliver_later
+        @user.update_attribute(:coins, @user.coins + @current_money/@winners_count)
+        winner.update_attribute(:bet_state, 1)
+      end
     end
+
     ## Close match
     @match.state = 1
     @match.save
