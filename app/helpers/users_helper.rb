33f9ca47f15128cdb78d1091e00d8eac776c6919
Antonio López <alopez7@uc.cl>
2017-06-23 17:19:05 -0400

    paleta y botones mejorados

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -8,9 +8,9 @@ module UsersHelper
       relation.follower_id == current_user.id
     end
     if followed_relation
-      link_to 'Unfollow', follow_path(followed_relation), method: 'delete', remote: true, data: { type: 'json', 'user-id': user.id }
+      link_to 'Unfollow', follow_path(followed_relation), method: 'delete', remote: true, data: { type: 'json', 'user-id': user.id }, class: "link-button"
     else
-      link_to 'Follow', user_follows_path(user), method: 'post', remote: true, data: { type: 'json', 'user-id': user.id }
+      link_to 'Follow', user_follows_path(user), method: 'post', remote: true, data: { type: 'json', 'user-id': user.id }, class: "link-button"
     end
   end
 end
