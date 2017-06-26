4d8e6f40aa8e12936f472f6426a48fb7aae757d0
Pablo <pfnieto@uc.cl>
2017-06-26 00:46:48 -0400

    remove dummy files

--- app/assets/javascripts/pagination.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-  $(".pagination a").live("click", function() {
-    alert("HOLA");
-    //$(".pagination").html("Page is loading...");
-    $.get(this.href, null, null, "script");
-    return false;
-  });
-});
