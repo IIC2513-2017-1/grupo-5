02c9cb47073fbda52026456b4cd4b42767e61d17
Pablo Nieto <pfnieto@uc.cl>
2017-07-03 16:50:52 -0400

    Fix following AJAX (#44)

--- app/assets/javascripts/follows.js
+++ app/assets/javascripts/follows.js
@@ -21,4 +21,22 @@ $(document).on('turbolinks:load', function () {
       console.log(data);
     });
   }
+  if (($events = $('.events')).length) {
+    $events.on('ajax:success', function (e, data) {
+      var $button;
+      if (data.followed_id) {
+        $button = $('.information [data-user-id="' + data.followed_id + '"]');
+        $button.data('method', 'delete');
+        $button.attr('href', '/follows/' + data.follow_id);
+        $button.text('Unfollow');
+      } else {
+        $button = $('.information [data-user-id="' + data.unfollowed_id + '"]');
+        $button.data('method', 'post');
+        $button.attr('href', '/users/' + data.unfollowed_id + '/follows');
+        $button.text('Follow');
+      }
+    }).on('ajax:error', function (e, data) {
+      console.log(data);
+    });
+  }
 });
